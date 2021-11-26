beeb4ee3dadd7b259487fb6df983e16702b5c202
ismailherianto <ianlotus35@gmail.com>
2021-11-26 20:39:07 +0800

    database

---
+++ db_bettafish.sql
@@ -0,0 +1,267 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 26, 2021 at 01:38 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bettafish`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2021_11_19_124335_create_barangs_table', 2),
+(3, '2021_11_19_124400_create_m_lelangs_table', 2),
+(4, '2021_11_19_124448_create_m_tokos_table', 2),
+(5, '2021_11_19_124510_create_m_info_lelangs_table', 2),
+(6, '2021_11_25_075603_create_penawarans_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_barang`
+--
+
+CREATE TABLE `t_barang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `brand` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `harga` int(11) NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_infolelang`
+--
+
+CREATE TABLE `t_infolelang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `foto` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `deskripsi` text COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_lelang`
+--
+
+CREATE TABLE `t_lelang` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `brand` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `keterangan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tgl_buka` datetime NOT NULL DEFAULT current_timestamp(),
+  `tgl_tutup` datetime NOT NULL,
+  `harga_buka` int(11) NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto2` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `foto3` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `video` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '1',
+  `kode_lelang` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_penawaran`
+--
+
+CREATE TABLE `t_penawaran` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `harga_tawar` int(11) NOT NULL,
+  `pending` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `user_id` int(11) DEFAULT NULL,
+  `lelang_id` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `t_tentangtoko`
+--
+
+CREATE TABLE `t_tentangtoko` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `foto` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `kontak` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `instagram` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `passStr` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `kontak` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `alamat` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `role` enum('0','1') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
+  `instagram` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `email`, `password`, `passStr`, `kontak`, `foto`, `alamat`, `status`, `role`, `instagram`) VALUES
+(1, 'Administrator', 'admin@gmail.com', '$2y$10$uzJvflmQj7kB7zVsDMtFpeUgbz56cMaE0DvhrE/GtIt7eDjPmRhqm', '', '085667889878', NULL, 'tes alamat', '1', '1', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_barang`
+--
+ALTER TABLE `t_barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_infolelang`
+--
+ALTER TABLE `t_infolelang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_lelang`
+--
+ALTER TABLE `t_lelang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `t_penawaran`
+--
+ALTER TABLE `t_penawaran`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `t_penawaran_user_id_foreign` (`user_id`),
+  ADD KEY `t_penawaran_lelang_id_foreign` (`lelang_id`);
+
+--
+-- Indexes for table `t_tentangtoko`
+--
+ALTER TABLE `t_tentangtoko`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `t_user_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `t_barang`
+--
+ALTER TABLE `t_barang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `t_infolelang`
+--
+ALTER TABLE `t_infolelang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `t_lelang`
+--
+ALTER TABLE `t_lelang`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `t_penawaran`
+--
+ALTER TABLE `t_penawaran`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `t_tentangtoko`
+--
+ALTER TABLE `t_tentangtoko`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `t_penawaran`
+--
+ALTER TABLE `t_penawaran`
+  ADD CONSTRAINT `t_penawaran_lelang_id_foreign` FOREIGN KEY (`lelang_id`) REFERENCES `t_lelang` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `t_penawaran_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
